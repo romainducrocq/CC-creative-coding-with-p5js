1afd9c98d134d27e1b9e95756b1da6475a79a8de
romainducrocq <romain.g.ducrocq@gmail.com>
2021-02-06 17:08:11 +0100

    add template

---
+++ _template/sketch.js
@@ -0,0 +1,55 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+/*
+
+Div = createDiv("");
+Div.position(,);
+Div.style('font-size', '16px');
+Div.style('font-family','Ubuntu, sans-serif');
+Div.style('color',color(51));
+// let val = Div.html(); //get
+// Div.html(""); //set
+
+Button = createButton("");
+Button.position(,);
+Button.style('font-size', '16px');
+Button.style('font-family','Ubuntu, sans-serif');
+Button.style('color',color(51));
+Button.mousePressed(function);
+
+Slider = createSlider(1, 3, 2, 1);
+Slider.position(,);
+// let val = Slider.value(); //get
+// Slider.value(int); //set
+
+Input = createInput("");
+Input.position(,);
+Input.size(50);
+Input.value("");
+// let val = Input.value(); //get
+// Input.value(""); //set
+
+Checkbox = createCheckbox("", boolean);
+Checkbox.position(,);
+// let val = Checkbox.checked(); //get
+// Checkbox.checked(boolean); //set
+
+function keyPressed() {
+  if(keyCode === 80){ //P
+  }else if(keyCode === 32){ //SPACE
+  }else if(keyCode === UP_ARROW){
+  }else if (keyCode === DOWN_ARROW){
+  }else if (keyCode === LEFT_ARROW){
+  }else if (keyCode === RIGHT_ARROW){
+  }
+}
+
+function mousePressed() {
+}
+*/
